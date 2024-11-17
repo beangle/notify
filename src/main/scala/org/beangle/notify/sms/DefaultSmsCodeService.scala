9c324a356f31daf4a254e33286bf7f9bfb5dd052
段体华 <duantihua@gmail.com>
2024-11-17 18:02:21 +0800

    update to commons cache

--- src/main/scala/org/beangle/notify/sms/DefaultSmsCodeService.scala
+++ src/main/scala/org/beangle/notify/sms/DefaultSmsCodeService.scala
@@ -17,7 +17,7 @@
 
 package org.beangle.notify.sms
 
-import org.beangle.cache.Cache
+import org.beangle.commons.cache.Cache
 import org.beangle.cache.caffeine.CaffeineCacheManager
 import org.beangle.commons.lang.Strings
 
